71120ffe7a7596c786823b20504d77b74b812511
Simon <simonbakkenjantasuk@pm.me>
2024-11-05 15:28:28 +0100

    Add react fragment

--- src/App.js
+++ src/App.js
@@ -29,9 +29,11 @@ export default function Game() {
       description = "Go to game start";
     }
     return (
-      <li key={move}>
-        <button onClick={() => jumpTo(move)}>{description}</button>
-      </li>
+      <>
+        <li key={move}>
+          <button onClick={() => jumpTo(move)}>{description}</button>
+        </li>
+      </>
     );
   });
 
