80b5aa7f659d43ef54078b57ef27a9d23827e183
Simon <simonbakkenjantasuk@pm.me>
2024-11-21 22:03:53 +0100

    Change origin and URL to current heroku URL

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -2,6 +2,7 @@ import { io } from 'socket.io-client';
 
 // "undefined" means the URL will be computed from the `window.location` object
 // const URL = process.env.NODE_ENV === 'production' ? undefined : 'http://localhost:4000';
-const URL = process.env.CLIENT_URL || "http://localhost:4000"
+// const URL = process.env.CLIENT_URL || "http://localhost:4000"
 
+const URL = "https://tic-tac-toe-react-socketio-03fdff0edcf9.herokuapp.com"
 export const socket = io(URL);
