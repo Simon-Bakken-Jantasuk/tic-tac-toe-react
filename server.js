9512e7fc507b04d761bb05aa33487cdadd0cfc8d
Simon <simonbakkenjantasuk@pm.me>
2024-11-21 22:15:14 +0100

    Add port to arg

--- server.js
+++ server.js
@@ -94,6 +94,6 @@ io.on("connection", (socket) => {
 });
 
 const port = process.env.PORT || 4000
-server.listen(4000, () => {
+server.listen(port, () => {
   console.log("Server is running on port 4000");
 });
