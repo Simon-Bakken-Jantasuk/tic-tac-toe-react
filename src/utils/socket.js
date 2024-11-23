86c0fb20e1f3d90be10734a4cbf0ea6dcf12a532
Simon <simonbakkenjantasuk@pm.me>
2024-11-23 05:56:24 +0100

    Add deployment

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,7 +1,5 @@
 import { io } from 'socket.io-client';
 
-const socketURL = process.env.NODE_ENV === "production"
-  ? `wss://mysterious-ravine-23326-f566692a40b9.herokuapp.com/` 
-  : "http://localhost:4000"; 
+const socketURL = process.env.NODE_ENV === "production" ? `wss://mysterious-ravine-23326-f566692a40b9.herokuapp.com/` : "http://localhost:4000"; 
 
 export const socket = io(socketURL);
