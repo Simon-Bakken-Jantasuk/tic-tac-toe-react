b82eac954fa6059be3f4729f21cf403d73a6b3ae
Simon <simonbakkenjantasuk@pm.me>
2024-11-23 01:35:24 +0100

    Add URL to socket.js

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,4 +1,5 @@
 import { io } from 'socket.io-client';
 
-const URL = process.env.REACT_APP_SOCKET_URL || "http://localhost:4000" 
+// const URL = process.env.REACT_APP_SOCKET_URL || "http://localhost:4000" 
+const URL = `wss://tic-tac-toe-react-socketio-03fdff0edcf9.herokuapp.com:${process.env.PORT}` || "http://localhost:4000"
 export const socket = io(URL);
